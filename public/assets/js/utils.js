ab563e2c9c8a67d65c9e3b188451668ee20a1a7e
gabzdev <gabz@gabzdev.ch>
2025-04-15 21:58:49 +0200

    feat: Add truncateText function

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,4 +1,5 @@
 const formatCurrency = (amount) => "$" + amount.toLocaleString();
 const formatNumber = (num) => num >= 1_000_000 ? (num / 1_000_000).toFixed(2) + "M" : num >= 1_000 ? (num / 1_000).toFixed(2) + "K" : num;
+const truncateText = (text) => text.length > 10 ? text.substring(0, 10) + '...' : text;
 
-export { formatCurrency, formatNumber }
+export { formatCurrency, formatNumber, truncateText }

acf89fb703f6377b47df267fc5d68f8a72f4609a
gabzdev <gabz@gabzdev.ch>
2025-04-15 22:13:38 +0200

    feat: Add handleEmptyText function

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,5 +1,6 @@
 const formatCurrency = (amount) => "$" + amount.toLocaleString();
 const formatNumber = (num) => num >= 1_000_000 ? (num / 1_000_000).toFixed(2) + "M" : num >= 1_000 ? (num / 1_000).toFixed(2) + "K" : num;
-const truncateText = (text) => text.length > 10 ? text.substring(0, 10) + '...' : text;
+const truncateText = (text, max) => text.length > max ? text.substring(0, max) + '...' : text;
+const handleEmptyText = (text) => value && value.trim() !== "" ? truncateText(value, 22) : " ";
 
-export { formatCurrency, formatNumber, truncateText }
+export { formatCurrency, formatNumber, truncateText, handleEmptyText }

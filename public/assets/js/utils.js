bfd5f44eeab56225fb8e1fd1954f490ec610f10f
gabzdev <gabz@gabzdev.ch>
2025-04-15 22:15:06 +0200

    fix: handleEmptyText function

--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -1,6 +1,6 @@
 const formatCurrency = (amount) => "$" + amount.toLocaleString();
 const formatNumber = (num) => num >= 1_000_000 ? (num / 1_000_000).toFixed(2) + "M" : num >= 1_000 ? (num / 1_000).toFixed(2) + "K" : num;
 const truncateText = (text, max) => text.length > max ? text.substring(0, max) + '...' : text;
-const handleEmptyText = (text) => value && value.trim() !== "" ? truncateText(value, 22) : " ";
+const handleEmptyText = (text) => text && text.trim() !== "" ? truncateText(text, 22) : "Empty";
 
 export { formatCurrency, formatNumber, truncateText, handleEmptyText }
